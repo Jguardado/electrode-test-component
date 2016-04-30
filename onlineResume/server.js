d9bdd7d4fb658475f4029dabfbba9cc17330a476
Juan <jguardado85@gmail.com>
2016-04-29 23:22:54 -0700

    improving package.json

--- onlineResume/server.js
+++ onlineResume/server.js
@@ -3,5 +3,5 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 console.log('server is running');
-app.use(express.static('index.html'));
+app.use(express.static('/index.html'));
 app.listen(port);

b75b0640e3e6db124ce72bbfa5817712c6b75151
Juan <jguardado85@gmail.com>
2016-04-29 23:08:58 -0700

    upgraded everything

--- onlineResume/server.js
+++ onlineResume/server.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+console.log('server is running');
 app.use(express.static('index.html'));
 app.listen(port);
